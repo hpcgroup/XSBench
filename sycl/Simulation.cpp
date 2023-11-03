d80271809b0bcd19f6a9d8e098f4453e37355f91
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-11-03 19:00:54 -0400

    feat: update to SYCL2020 and synchronize with get_host_access()

--- sycl/Simulation.cpp
+++ sycl/Simulation.cpp
@@ -12,9 +12,7 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 // use SYCL namespace to reduce symbol names
-using namespace sycl;
-unsigned long long run_event_based_simulation(Inputs in, SimulationData SD, int mype, double * kernel_init_time)
-{
+unsigned long long run_event_based_simulation(Inputs in, SimulationData SD, int mype, double * kernel_init_time) {
 	
 	////////////////////////////////////////////////////////////////////////////////
 	// SUMMARY: Simulation Data Structure Manifest for "SD" Object
@@ -41,132 +39,111 @@ unsigned long long run_event_based_simulation(Inputs in, SimulationData SD, int 
 	
 	// Timers
 	double start = get_time();
-	double stop;
+	double stop = 0.0;
 
-	// Scope here is important, as when we exit this blocl we will automatically sync with device
-	// to ensure all work is done and that we can read from verification_host array.
-	{
-		// create a queue using the default device for the platform (cpu, gpu)
+    // create a queue using the default device for the platform (cpu, gpu)
 
-		queue sycl_q{default_selector_v};
-		if(mype == 0 ) printf("Running on: %s\n", sycl_q.get_device().get_info<sycl::info::device::name>().c_str());
-		if(mype == 0 ) printf("Initializing device buffers and JIT compiling kernel...\n");
+    sycl::queue sycl_q{sycl::default_selector_v};
+    if(mype == 0 ) printf("Running on: %s\n", sycl_q.get_device().get_info<sycl::info::device::name>().c_str());
+    if(mype == 0 ) printf("Initializing device buffers and JIT compiling kernel...\n");
 	
-		////////////////////////////////////////////////////////////////////////////////
-		// Create Device Buffers
-		////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    // Create Device Buffers
+    ////////////////////////////////////////////////////////////////////////////////
 
-		// assign SYCL buffer to existing memory
-		buffer<int, 1> num_nucs_d(SD.num_nucs,SD.length_num_nucs);
-		buffer<double, 1> concs_d(SD.concs, SD.length_concs);
-		buffer<int, 1> mats_d(SD.mats, SD.length_mats);
-		buffer<NuclideGridPoint, 1> nuclide_grid_d(SD.nuclide_grid, SD.length_nuclide_grid);
-		buffer<int, 1> verification_d(verification_host, in.lookups);
+    // assign SYCL buffer to existing memory
+    sycl::buffer<int> num_nucs_d(SD.num_nucs,SD.length_num_nucs);
+    sycl::buffer<double> concs_d(SD.concs, SD.length_concs);
+    sycl::buffer<int> mats_d(SD.mats, SD.length_mats);
+    sycl::buffer<double> unionized_energy_array_d(SD.unionized_energy_array, SD.length_unionized_energy_array);
+    sycl::buffer<int> index_grid_d(SD.index_grid, SD.length_index_grid);
+    sycl::buffer<NuclideGridPoint> nuclide_grid_d(SD.nuclide_grid, SD.length_nuclide_grid);
+    sycl::buffer<int> verification_d(verification_host, in.lookups);
 
-		// These two are a bit of a hack. Sometimes they are empty buffers (if using hash or nuclide
-		// grid methods). OpenCL will throw an example when we try to create an empty buffer. So, we
-		// will just allocate some memory for them and move them as normal. The rest of our code
-		// won't actually use them if they aren't needed, so this is safe. Probably a cleaner way
-		// of doing this.
-		if( SD.length_unionized_energy_array == 0 )
-		{
-			SD.length_unionized_energy_array = 1;
-			SD.unionized_energy_array = (double *) malloc(sizeof(double));
-		}
-		buffer<double,1> unionized_energy_array_d(SD.unionized_energy_array, SD.length_unionized_energy_array);
-		if( SD.length_index_grid == 0 )
-		{
-			SD.length_index_grid = 1;
-			SD.index_grid = (int *) malloc(sizeof(int));
-		}
-		// For the unionized grid, this is a large array. Enforce that it is able to be allocated on the
-		// OpenCL device (as some OpenCL devices have fairly low limts here for some reason...)
-		size_t index_grid_allocation_sz = ceil((SD.length_index_grid * sizeof(int)));
-		assert( index_grid_allocation_sz <= sycl_q.get_device().get_info<sycl::info::device::max_mem_alloc_size>() );
-		buffer<int, 1> index_grid_d(SD.index_grid, (unsigned long long ) SD.length_index_grid);
 		
-		////////////////////////////////////////////////////////////////////////////////
-		// Define Device Kernel
-		////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    // Define Device Kernel
+    ////////////////////////////////////////////////////////////////////////////////
+    
+    if(mype==0) printf("Beginning event based simulation...\n");
 
-		// queue a kernel to be run, as a lambda
-		sycl_q.submit([&](handler &cgh)
-				{
-				////////////////////////////////////////////////////////////////////////////////
-				// Create Device Accessors for Device Buffers
-				////////////////////////////////////////////////////////////////////////////////
-				auto num_nucs = num_nucs_d.get_access<access::mode::read>(cgh);
-				auto concs = concs_d.get_access<access::mode::read>(cgh);
-				auto mats = mats_d.get_access<access::mode::read>(cgh);
-				auto nuclide_grid = nuclide_grid_d.get_access<access::mode::read>(cgh);
-				auto verification = verification_d.get_access<access::mode::write>(cgh);
-				auto unionized_energy_array = unionized_energy_array_d.get_access<access::mode::read>(cgh);
-				auto index_grid = index_grid_d.get_access<access::mode::read>(cgh);
+    // queue a kernel to be run, as a lambda
+    sycl_q.submit([&](handler &cgh) {
+        ////////////////////////////////////////////////////////////////////////////////
+        // Create Device Accessors for Device Buffers
+        ////////////////////////////////////////////////////////////////////////////////
+        sycl::accessor num_nucs {num_nucs_d, cgh, sycl::read_only};
+        sycl::accessor concs {concs_d, cgh, sycl::read_only};
+        sycl::accessor mats {mats_d, cgh, sycl::read_only};
+        sycl::accessor nuclide_grid {nuclide_grid_d, cgh, sycl::read_only};
+        sycl::accessor unionized_energy_array {unionized_energy_array_d, cgh, sycl::read_only};
+        sycl::accessor index_grid {index_grid_d, cgh, sycl::read_only};
+        sycl::accessor verification {verification_d, sycl::write_only, sycl::no_init};
 
-				////////////////////////////////////////////////////////////////////////////////
-				// XS Lookup Simulation Loop
-				////////////////////////////////////////////////////////////////////////////////
-				cgh.parallel_for<kernel>(range<1>(in.lookups), [=](id<1> idx)
-					{
-					// get the index to operate on, first dimemsion
-					size_t i = idx[0];
+        ////////////////////////////////////////////////////////////////////////////////
+        // XS Lookup Simulation Loop
+        ////////////////////////////////////////////////////////////////////////////////
+        cgh.parallel_for<sycl::kernel>(sycl::range<1>(in.lookups), [=](sycl::id<1> idx) {
+            // get the index to operate on, first dimemsion
+            size_t i = idx[0];
 
-					// Set the initial seed value
-					uint64_t seed = STARTING_SEED;	
+            // Set the initial seed value
+            uint64_t seed = STARTING_SEED;	
 
-					// Forward seed to lookup index (we need 2 samples per lookup)
-					seed = fast_forward_LCG(seed, 2*i);
+            // Forward seed to lookup index (we need 2 samples per lookup)
+            seed = fast_forward_LCG(seed, 2*i);
 
-					// Randomly pick an energy and material for the particle
-					double p_energy = LCG_random_double(&seed);
-					int mat         = pick_mat(&seed); 
+            // Randomly pick an energy and material for the particle
+            double p_energy = LCG_random_double(&seed);
+            int mat         = pick_mat(&seed); 
 
-					// debugging
-					//printf("E = %lf mat = %d\n", p_energy, mat);
+            // debugging
+            //printf("E = %lf mat = %d\n", p_energy, mat);
 
-					double macro_xs_vector[5] = {0};
+            double macro_xs_vector[5] = {0};
 
-					// Perform macroscopic Cross Section Lookup
-					calculate_macro_xs(
-							p_energy,        // Sampled neutron energy (in lethargy)
-							mat,             // Sampled material type index neutron is in
-							in.n_isotopes,   // Total number of isotopes in simulation
-							in.n_gridpoints, // Number of gridpoints per isotope in simulation
-							num_nucs,     // 1-D array with number of nuclides per material
-							concs,        // Flattened 2-D array with concentration of each nuclide in each material
-							unionized_energy_array, // 1-D Unionized energy array
-							index_grid,   // Flattened 2-D grid holding indices into nuclide grid for each unionized energy level
-							nuclide_grid, // Flattened 2-D grid holding energy levels and XS_data for all nuclides in simulation
-							mats,         // Flattened 2-D array with nuclide indices defining composition of each type of material
-							macro_xs_vector, // 1-D array with result of the macroscopic cross section (5 different reaction channels)
-							in.grid_type,    // Lookup type (nuclide, hash, or unionized)
-							in.hash_bins,    // Number of hash bins used (if using hash lookup type)
-							SD.max_num_nucs  // Maximum number of nuclides present in any material
-							);
+            // Perform macroscopic Cross Section Lookup
+            calculate_macro_xs(
+                p_energy,        // Sampled neutron energy (in lethargy)
+                mat,             // Sampled material type index neutron is in
+                in.n_isotopes,   // Total number of isotopes in simulation
+                in.n_gridpoints, // Number of gridpoints per isotope in simulation
+                num_nucs,     // 1-D array with number of nuclides per material
+                concs,        // Flattened 2-D array with concentration of each nuclide in each material
+                unionized_energy_array, // 1-D Unionized energy array
+                index_grid,   // Flattened 2-D grid holding indices into nuclide grid for each unionized energy level
+                nuclide_grid, // Flattened 2-D grid holding energy levels and XS_data for all nuclides in simulation
+                mats,         // Flattened 2-D array with nuclide indices defining composition of each type of material
+                macro_xs_vector, // 1-D array with result of the macroscopic cross section (5 different reaction channels)
+                in.grid_type,    // Lookup type (nuclide, hash, or unionized)
+                in.hash_bins,    // Number of hash bins used (if using hash lookup type)
+                SD.max_num_nucs  // Maximum number of nuclides present in any material
+            );
 
-					// For verification, and to prevent the compiler from optimizing
-					// all work out, we interrogate the returned macro_xs_vector array
-					// to find its maximum value index, then increment the verification
-					// value by that index. In this implementation, we store to a global
-					// array that will get tranferred back and reduced on the host.
-					double max = -1.0;
-					int max_idx = 0;
-					for(int j = 0; j < 5; j++ )
-					{
-						if( macro_xs_vector[j] > max )
-						{
-							max = macro_xs_vector[j];
-							max_idx = j;
-						}
-					}
-					verification[i] = max_idx+1;
+            // For verification, and to prevent the compiler from optimizing
+            // all work out, we interrogate the returned macro_xs_vector array
+            // to find its maximum value index, then increment the verification
+            // value by that index. In this implementation, we store to a global
+            // array that will get tranferred back and reduced on the host.
+            double max = -1.0;
+            int max_idx = 0;
+            for(int j = 0; j < 5; j++ )
+            {
+                if( macro_xs_vector[j] > max )
+                {
+                    max = macro_xs_vector[j];
+                    max_idx = j;
+                }
+            }
+            verification[i] = max_idx+1;
 
-					});
-				});
-		stop = get_time();
-		if(mype==0) printf("Kernel initialization, compilation, and launch took %.2lf seconds.\n", stop-start);
-		if(mype==0) printf("Beginning event based simulation...\n");
-	}
+        });
+    });
+    stop = get_time();
+    
+    if(mype==0) printf("Kernel initialization, compilation, and launch took %.2lf seconds.\n", stop-start);
+
+    verification_d.get_host_acess();
 
 	// Host reduces the verification array
 	unsigned long long verification_scalar = 0;
